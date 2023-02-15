a828c785c45f6578fdfa88376843474dc68dba2f
Unknown <matthijsvrenswoude@gmail.com>
2023-02-15 04:09:02 +0100

    Added topics

--- romdrop.com/docusaurus.config.js
+++ romdrop.com/docusaurus.config.js
@@ -55,9 +55,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Romdrop',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Romdrop Logo',
           src: 'img/logo.svg',
         },
         items: [
